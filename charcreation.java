cbf0e7ac7126da7d3fcd8f9f25ac0b4c6c40f51d
briankong18 <brian.kong18@ycdsbk12.ca>
2017-05-17 12:29:43 -0400

    Add files via upload

--- charcreation.java
+++ charcreation.java
@@ -7,11 +7,118 @@ public class charcreation{
     //gamebackground
     con.setColor(new Color(99,99,99));
     con.fillRect(0,0,10000,10000);
+    con.setColor(new Color(58,24,20));
+    //left door
+    con.fillRect(350,390,50,50);
+    con.fillRect(350,340,50,50);
+    con.fillOval(338,340,25,100);
+    //forward door
+    con.fillRect(650,40,50,50);
+    con.fillRect(700,40,50,50);
+    con.fillOval(650,28,100,25);
+    //right door
+    con.fillRect(1000,390,50,50);
+    con.fillRect(1000,340,50,50);
+    con.fillOval(1038,340,25,100);
+    //back door
+    con.fillRect(650,690,50,50);
+    con.fillRect(700,690,50,50);
+    con.fillOval(650,728,100,25);
+    con.setColor(new Color(0,0,0));
+    //left door
+    con.drawRect(350,390,50,50);
+    con.drawRect(350,340,50,50);
+    con.drawRect(350,390,50,40);
+    con.drawRect(350,390,50,30);
+    con.drawRect(350,390,50,20);
+    con.drawRect(350,390,50,10);
+    con.drawRect(350,340,50,40);
+    con.drawRect(350,340,50,30);
+    con.drawRect(350,340,50,20);
+    con.drawRect(350,340,50,10);
+    //forward door
+    con.drawRect(650,40,50,50);
+    con.drawRect(700,40,50,50);
+    con.drawRect(650,40,40,50);
+    con.drawRect(650,40,30,50);
+    con.drawRect(650,40,20,50);
+    con.drawRect(650,40,10,50);
+    con.drawRect(700,40,40,50);
+    con.drawRect(700,40,30,50);
+    con.drawRect(700,40,20,50);
+    con.drawRect(700,40,10,50);
+    //right door
+    con.drawRect(1000,390,50,50);
+    con.drawRect(1000,340,50,50);
+    con.drawRect(1000,390,50,40);
+    con.drawRect(1000,390,50,30);
+    con.drawRect(1000,390,50,20);
+    con.drawRect(1000,390,50,10);
+    con.drawRect(1000,340,50,40);
+    con.drawRect(1000,340,50,30);
+    con.drawRect(1000,340,50,20);
+    con.drawRect(1000,340,50,10);
+    //back door
+    con.drawRect(650,690,50,50);
+    con.drawRect(700,690,50,50);
+    con.drawRect(650,690,40,50);
+    con.drawRect(650,690,30,50);
+    con.drawRect(650,690,20,50);
+    con.drawRect(650,690,10,50);
+    con.drawRect(700,690,40,50);
+    con.drawRect(700,690,30,50);
+    con.drawRect(700,690,20,50);
+    con.drawRect(700,690,10,50);
+    con.setColor(new Color(252,194,74));
+    //left door
+    con.fillRect(365,378,12,24);
+    //forward door
+    con.fillRect(688,55,24,12);
+    //right door
+    con.fillRect(1023,378,12,24);
+    //back door
+    con.fillRect(688,715,24,12);
+    con.setColor(new Color(0,0,0));
+    con.fillOval(368,387,6,6);
+    con.fillOval(697,58,6,6);
+    con.fillOval(1026,387,6,6);
+    con.fillOval(697,718,6,6);
+    
+    con.fillRect(372,389,4,2);
+    con.fillRect(699,62,2,4);
+    con.fillRect(1024,389,4,2);
+    con.fillRect(699,716,2,4);
+    
+    con.drawLine(310,0,9310,9000);
+    con.drawLine(310,780,1090,0);
+    con.drawLine(310,0,310,780);
+    con.drawLine(1090,0,1090,780);
+    
+    con.setColor(new Color(252,194,74));
+    con.fillRect(120,90,20,20);
+    con.fillRect(210,90,20,20);
+    con.setColor(new Color(191,121,76));
+    con.fillRoundRect(75,140,200,300,20,20);
+    con.setColor(new Color(153,83,48));
+    con.fillRoundRect(75,240,200,300,50,50);;
+    con.fillRect(275,400,20,100);
+    con.fillRect(55,400,20,100);
+    con.fillRect(125,40,10,100);
+    con.fillRect(215,40,10,100);
+    
+    con.setColor(new Color(0,0,0));
+    con.drawRect(100,290,150,200);
+    con.drawRect(100,290,150,50);
+    con.drawRect(100,290,150,100);
+    con.drawRect(100,290,150,150);
+    con.drawRect(100,290,50,200);
+    con.drawRect(100,290,100,200);
     
     //game boarder
     con.setColor(new Color(125,118,113));
     con.fillRect(400,90,600,600);
     con.setColor(new Color(213,213,213));
+    con.drawRect(400,90,600,600);
     con.drawRect(400,90,550,600);
     con.drawRect(400,90,500,600);
     con.drawRect(400,90,450,600);
@@ -48,48 +155,5 @@ public class charcreation{
     con.setColor(new Color(0,0,0));
     con.fillOval(715,405,6,6);
     con.fillOval(728,405,6,6);
-    
-    con.setColor(new Color(56,47,47));
-    //berserker
-    con.setColor(new Color(93,55,28));
-    con.fillOval(610,292,8,8);
-    con.fillOval(632,292,8,8);
-    con.setColor(new Color(46,43,43));
-    con.fillOval(605,295,40,40);
-    con.setColor(new Color(183,6,6));
-    int xpoints1[] = {612,624,637,637,625,612};
-    int ypoints1[] = {305,300,305,311,306,311};
-    int npoints1 = 6;
-    con.fillPolygon(xpoints1, ypoints1, npoints1);
-    
-    //knight
-    con.setColor(new Color(93,55,28));
-    con.fillOval(510,292,8,8);
-    con.fillOval(532,292,8,8);
-    con.setColor(new Color(42,55,68));
-    con.fillOval(505,295,40,40);
-    con.setColor(new Color(249,207,178));
-    int xpoints2[] = {512,525,538,538,525,512};
-    int ypoints2[] = {300,295,300,315,310,315};
-    int npoints2 = 6;
-    con.fillPolygon(xpoints2, ypoints2, npoints2);
-    con.setColor(new Color(0,0,0));
-    con.fillOval(515,305,6,6);
-    con.fillOval(528,305,6,6);
-    con.setColor(new Color(42,55,68));
-    con.fillRect(523,302,4,10);
-    
-    con.setColor(new Color(93,55,28));
-    con.fillOval(810,292,8,8);
-    con.fillOval(832,292,8,8);
-    con.setColor(new Color(255,255,255));
-    con.fillOval(805,295,40,40);
-    con.setColor(new Color(0,52,116));
-    int xpoints3[] = {806,812,815,818,824,831,834,837,843,833,832,830,825,818,816,815};
-    int ypoints3[] = {311,309,302,306,300,306,302,309,311,310,314,308,306,308,314,310};
-    int npoints3 = 16;
-    con.fillPolygon(xpoints3, ypoints3, npoints3);
-    
-    
   }
 }
